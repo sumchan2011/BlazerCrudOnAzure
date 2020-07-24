3eef19410dc2f8e740c663a263e8a4063e61927e
Sum Chan <sumchan2011@gmail.com>
2020-07-25 00:16:29 +0800

    added login layout

---
+++ BlazorCRUD1/Contracts/IUserManager.cs
@@ -0,0 +1,17 @@
+﻿using BlazorCRUD1.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD1.Contracts
+{
+    public interface ICustomerManager
+    {
+        Task<int> Create(Customer customer);
+        Task<int> Delete(int Id);
+        Task<int> Count(string search);
+        Task<int> Update(Customer customer);
+        Task<Customer> GetById(int Id);
+        Task<List<Customer>> ListAll(string customerPhone);
+        Task<List<Customer>> ListAll(int skip, int take, string orderBy, string direction, string customerPhone);
+    }
+}
